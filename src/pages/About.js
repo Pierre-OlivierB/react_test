4b5057ee7fe8e73ecdc36f39f53fcc468de66b25
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2022-12-06 12:24:46 +0100

    react discover

---
+++ src/pages/About.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, alias
+        neque! Quisquam ducimus eos, pariatur laborum, labore architecto
+        provident cum magni voluptas delectus ex? Tempora optio eligendi
+        aspernatur minima numquam enim officiis, voluptate a quaerat nemo
+        pariatur molestias quo deserunt modi praesentium earum similique sequi
+        accusamus eos distinctio officia consequatur beatae quis! Numquam iure
+        at nihil quisquam nisi nam, reprehenderit modi corporis excepturi
+        blanditiis repellat? Est facilis aliquid quos rem perferendis! Ea
+        distinctio optio earum ex, eaque voluptatum, hic accusamus repudiandae
+        omnis nam delectus laboriosam itaque ratione iusto. Error tempora porro
+        consequuntur suscipit temporibus dolorum velit harum! Doloremque,
+        officiis saepe?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quasi
+        non neque modi itaque consequuntur sit nisi pariatur eius veritatis
+        blanditiis totam, facere rem voluptate at id. Voluptate cumque omnis
+        assumenda nesciunt corrupti explicabo, est minima cupiditate quibusdam
+        rem vero, aperiam laboriosam. Quas doloremque nisi, dolorum similique
+        enim quia atque, sunt saepe explicabo hic provident consequatur repellat
+        autem dolore temporibus accusamus praesentium itaque a nemo voluptas
+        quae! Incidunt omnis, fugit odio beatae voluptas ducimus harum.
+        Repellat, alias earum sapiente harum fugiat non voluptates incidunt odio
+        natus esse ipsa voluptatum quam ad ea ex eveniet mollitia, unde
+        voluptatibus, id nulla dicta corrupti. Error eum dolor nemo doloremque
+        perferendis consectetur, aspernatur non facilis facere maxime! Soluta
+        doloremque maiores, cumque odit, aliquid dolor architecto delectus
+        blanditiis distinctio, debitis id porro officiis nam laudantium odio
+        voluptates iure animi eveniet reiciendis nihil. Necessitatibus sapiente
+        distinctio reprehenderit blanditiis asperiores impedit repellat nostrum,
+        possimus itaque soluta numquam deleniti. Voluptatibus modi error optio
+        consectetur quae deleniti distinctio tempore nulla rem ratione!
+        Praesentium, rem quas iure officiis, sunt voluptatibus quasi numquam
+        reiciendis molestiae qui libero est maiores doloremque perferendis
+        earum, commodi repellat animi aperiam fugit! Hic libero facere minima
+        ipsum! Consequatur ullam rerum vel sequi aut mollitia labore quidem?
+      </p>
+    </div>
+  );
+};
+
+export default About;
